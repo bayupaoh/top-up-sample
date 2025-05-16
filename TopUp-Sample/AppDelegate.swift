b72fc849a100b3fa047ee86174b06b798affb663
Bayu Paoh <bayu.paoh@aladinbank.id>
2025-05-16 15:59:33 +0700

    [NO-TICKET] Delete storyboard

--- TopUp-Sample/AppDelegate.swift
+++ TopUp-Sample/AppDelegate.swift
@@ -10,12 +10,17 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        window = UIWindow()
+        let viewController = SampleViewController()
+        let navigationController = UINavigationController(rootViewController: viewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
         return true
     }
+
 
     // MARK: UISceneSession Lifecycle
 
---
+++ TopUp-Sample/Presentation/Sample/SampleViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SampleViewController.swift
+//  TopUp-Sample
+//
+//  Created by Bayu Firmawan Paoh on 16/05/25.
+//
+
+import UIKit
+
+final class SampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
